6ce0bffce1c258bbbaff9950a39a81bc4a9ec340
Filip Janješić <filip.janjesic@gmail.com>
2023-10-04 21:59:35 +0200

    Update VehicleModelsController.cs

--- Controllers/VehicleModelsController.cs
+++ Controllers/VehicleModelsController.cs
@@ -1,32 +1,32 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using Autofac;
+using Autofac.Extensions.DependencyInjection;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using Mono.Data;
 using Mono.Models;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace Mono.Controllers
 {
     public class VehicleModelsController : Controller
     {
         private readonly VehicledbContext _context;
+        private readonly YourService _yourService;
 
-        public VehicleModelsController(VehicledbContext context)
+        public VehicleModelsController(VehicledbContext context, YourService yourService)
         {
             _context = context;
+            _yourService = yourService;
         }
 
-        // GET: VehicleModels
         public async Task<IActionResult> Index()
         {
             var vehicledbContext = _context.VehicleModels.Include(v => v.Make);
             return View(await vehicledbContext.ToListAsync());
         }
 
-        // GET: VehicleModels/Details/5
         public async Task<IActionResult> Details(int? id)
         {
             if (id == null || _context.VehicleModels == null)
@@ -45,16 +45,12 @@ namespace Mono.Controllers
             return View(vehicleModel);
         }
 
-        // GET: VehicleModels/Create
         public IActionResult Create()
         {
             ViewData["MakeId"] = new SelectList(_context.VehicleMakes, "Id", "Id");
             return View();
         }
 
-        // POST: VehicleModels/Create
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Create([Bind("Id,MakeId,Name,Abrv")] VehicleModel vehicleModel)
@@ -69,7 +65,6 @@ namespace Mono.Controllers
             return View(vehicleModel);
         }
 
-        // GET: VehicleModels/Edit/5
         public async Task<IActionResult> Edit(int? id)
         {
             if (id == null || _context.VehicleModels == null)
@@ -86,9 +81,6 @@ namespace Mono.Controllers
             return View(vehicleModel);
         }
 
-        // POST: VehicleModels/Edit/5
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Edit(int id, [Bind("Id,MakeId,Name,Abrv")] VehicleModel vehicleModel)
@@ -122,7 +114,6 @@ namespace Mono.Controllers
             return View(vehicleModel);
         }
 
-        // GET: VehicleModels/Delete/5
         public async Task<IActionResult> Delete(int? id)
         {
             if (id == null || _context.VehicleModels == null)
@@ -141,7 +132,6 @@ namespace Mono.Controllers
             return View(vehicleModel);
         }
 
-        // POST: VehicleModels/Delete/5
         [HttpPost, ActionName("Delete")]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> DeleteConfirmed(int id)
@@ -155,14 +145,22 @@ namespace Mono.Controllers
             {
                 _context.VehicleModels.Remove(vehicleModel);
             }
-            
+
             await _context.SaveChangesAsync();
             return RedirectToAction(nameof(Index));
         }
 
         private bool VehicleModelExists(int id)
         {
-          return (_context.VehicleModels?.Any(e => e.Id == id)).GetValueOrDefault();
+            return (_context.VehicleModels?.Any(e => e.Id == id)).GetValueOrDefault();
+        }
+
+        public class AutofacModule : Module
+        {
+            protected override void Load(ContainerBuilder builder)
+            {
+                builder.RegisterType<YourService>().AsSelf().InstancePerLifetimeScope();
+            }
         }
     }
 }
